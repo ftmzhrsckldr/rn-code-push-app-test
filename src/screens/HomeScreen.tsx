423c0948f225f539e5b6c89d26fa11e559b6a6b1
boztopuz <boztopuz@gmail.com>
2025-04-28 19:05:50 +0300

    Test v2 for CI step

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -153,7 +153,7 @@ const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.header}>
-        <Text style={styles.title}>Appcircle CodePush General Test, V4 CIstep test</Text>
+        <Text style={styles.title}>Appcircle CodePush General Test, V5 CI step test V2</Text>
         <Text style={styles.subtitle}>Current Version: {currentVersion}</Text>
 
         {updateAvailable && (
