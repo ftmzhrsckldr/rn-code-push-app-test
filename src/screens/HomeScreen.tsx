80f0187ca14fbc185076952ab7f0deac9791f53e
Zehra Saçaklıdır <zehra@appcircle.io>
2025-05-21 14:27:14 +0300

    Updated screen time

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -85,7 +85,7 @@ const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.header}>
-        <Text style={styles.title}>Appcircle CodePush Test 20 May 09:15</Text>
+        <Text style={styles.title}>Appcircle CodePush Test 21 May 14:26</Text>
         <Text style={styles.subtitle}>Current Version: {currentVersion}</Text>
       </View>
 
